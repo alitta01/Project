2b6870e2575cd2a95ed8415e02fa1622f3d7cb53
alitta01 <95178696+alitta01@users.noreply.github.com>
2022-01-25 22:12:07 +0530

    Update client.py

--- client.py
+++ client.py
@@ -13,9 +13,33 @@ while True:
     print(c.recv(1024).decode())
     print("\n")
     
-   if ch=='2':
+   elif ch=='2':
     c.send(bytes("2","utf-8"))
     f=input("Enter name of file:")
     c.send(bytes(f,"utf-8"))
     text=input("Enter the text to be added:")
+    c.send(bytes(text,"utf-8"))
+    print(c.recv(1024).decode())
+    print("\n")
     
+   elif ch=='3':
+     c.send(bytes("3","utf-8"))
+     f=input("Enter name of the file:")
+     c.send(bytes(f,"utf-8"))
+     print(c.recv(1024).decode())
+     print("\n")
+
+    elif ch=='4':
+      c.send(bytes("4","utf-8"))
+      f=input("Enter name of the file:")
+      c.send(bytes(f,"utf-8"))
+      print(c.recv(1024).decode())
+      print("\n")
+      
+     elif ch='5':
+      c.send(bytes("5","utf-8"))
+      print("Connection has been disconnted")
+      quit()
+      
+     else:
+      print("Please enter a valid choice!!")
