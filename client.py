b489ddb74432f707a31899b9e3a9ed6a62add9ab
alitta01 <95178696+alitta01@users.noreply.github.com>
2022-01-25 21:05:33 +0530

    Create client.py

---
+++ client.py
@@ -0,0 +1,17 @@
+import socket
+
+c=socket.socket()
+c.connect(('localhost',9999))
+print(c.recv(1024).decode())
+
+while True:
+  ch=input("1.Create File\n2.Edit File\n3.Delete File\n4.Read file\n5.End the connection\nEnter your choice:")
+  if ch=='1':
+    c.send(bytes("1","utf-8"))
+    f=input("Enter name of the file:")
+    c.send(bytes(f,"utf-8"))
+    print(c.recv(1024).decode())
+    print("\n")
+    
+   if ch=='2':
+    
